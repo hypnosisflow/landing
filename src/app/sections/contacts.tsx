250979a9decdc55469571b5bbe293212385ab763
Artem <plzv92@gmail.com>
2024-05-05 21:37:02 +0700

    add: new info

--- src/app/sections/contacts.tsx
+++ src/app/sections/contacts.tsx
@@ -6,16 +6,16 @@ import tg from "../assets/telegram.svg";
 export const Contacts = () => {
   return (
     <footer className="py-10 w-full flex flex-col">
-      <h2 className="text-2xl font-bold">Get In Touch</h2>
+      <h2 className="text-2xl font-medium">Get In Touch</h2>
 
-      <ul className="text-sm font-medium my-4  flex flex-col gap-1">
+      <ul className="text-xl font-light my-4  flex flex-col gap-1">
         <li className="opacity-50 hover:opacity-100">
           <Link
             href={"mailto:hypnosisflow@gmail"}
             target="_blank"
             className="flex gap-2 items-center"
           >
-            <Image src={email} width={20} height={20} alt="email-icon" />
+            {/* <Image src={email} width={20} height={20} alt="email-icon" /> */}
             <span>hypnosisflow@gmail.com</span>
           </Link>
         </li>
@@ -25,7 +25,7 @@ export const Contacts = () => {
             target="_blank"
             className="flex gap-2 items-center"
           >
-            <Image src={tg} width={20} height={20} alt="telegram-icon" />
+            {/* <Image src={tg} width={20} height={20} alt="telegram-icon" /> */}
             <span>hpnssflw</span>
           </Link>
         </li>
--- src/app/sections/experience.tsx
+++ src/app/sections/experience.tsx
@@ -5,81 +5,56 @@ import Link from "next/link";
 export const Experience = () => {
   return (
     <section className=" border-b py-10 w-full">
-      <h2 className="text-2xl font-bold ">Experience</h2>
+      <h2 className="text-2xl font-medium ">Experience</h2>
 
-      <div className="mt-4">
-        <h3 className="font-medium">Neiro.ai </h3>
-        <p className="text-xs font-light">
+      <div className="mt-8">
+        <h3 className="font-light text-xl">Neiro.ai </h3>
+        <p className="text-sm font-light">
           2023 - 2024 / San Francisco, CA. (remote)
         </p>
         <p className="text-sm  font-medium mt-2">Junior frontend developer</p>
-        <p className="my-2 text-sm font-light">
-          Assisted in the development of web applications using frontend stack
-          of <br />
-          <span className="font-medium">
+        <p className="mt-2 text-sm font-light">
+          Assisted in the development of web applications using Neiro AI generative technologies.
+        </p>
+        <span className="text-sm font-light">Techs:</span>
+        <span className="ml-2 text-sm font-medium">
             React, TailwindCSS + CSS Modules, MobX, Firebase, Figma
           </span>
-          .
-        </p>
-        {/* <Link to='' /> */}
-        <Link href="https://studio.neiro.ai" className="font-medium">
+        <br />
+        <Link href="https://studio.neiro.ai" className="font-medium mt-2 block" >
           {" "}
           <span className="font-light">&#8594;</span> https://studio.neiro.ai
         </Link>
-        <ul className="text-sm font-light">
-          <li>
-            <span> - Developed responsive UI according Figma designs.</span>
-          </li>
-          <li>
-            <span>- Used Redux for state management (migrated to MobX).</span>
-          </li>
-          <li>
-            <span>
-              - Integrated monetization (square.up) and developed UI for the
-              flow with adding animation using Particles.js.
-            </span>
-          </li>
-
-          <li>
-            <span>- SEO optimization using Lighthouse (90+).</span>
-          </li>
-          <li>
-            <span>- Integrated Firebase and Google Analytics.</span>
-          </li>
-          <li>
-            <span>
-              - Perfomance optimization with code splitting and lazy-loading.
-            </span>
-          </li>
-        </ul>
-
+       
         <Link href="https://neiro.ai" className="mt-2 block font-medium">
           {" "}
           <span className="font-light">&#8594;</span> https://neiro.ai
         </Link>
-        <ul className="text-sm font-light">
-          <li>
-            <span> - Developed responsive landing page according Figma.</span>
-          </li>
-          <li>
-            <span>
-              - Improved website accessibility following WCAG guidelines
-            </span>
-          </li>
-          <li>
-            <span>- Reached SEO scores of 95+ using Lighthouse.</span>
-          </li>
+  
 
-          <li>
-            <span>
-              - Intergrated Firebase to make users free to contact with Support
-              Team and Google Analytics for business purposes.
-            </span>
-          </li>
-          <li>
-            <span>- Used Anime.js for implementing animations.</span>
-          </li>
-        </ul>
+      </div>
+
+      
+      <div className="mt-8">
+        <h3 className="font-light text-xl">EGAR Technology </h3>
+        <p className="text-sm font-light">
+          2024 - current / Tomsk, Russia.
+        </p>
+        <p className="text-sm  font-medium mt-2">Frontend developer</p>
+        <p className="mt-2 text-sm font-light">
+          Assisted in the development of web applications using Neiro AI generative technologies.
+        </p>
+        <span className="text-sm font-light">Techs:</span>
+        <span className="ml-2 text-sm font-medium">
+            React, TailwindCSS + CSS Modules, MobX, Firebase, Figma
+          </span>
+        <Link href="https://data.moex.com/" className="mt-2 block font-medium">
+          {" "}
+          <span className="font-light">&#8594;</span> https://data.moex.com/
+        </Link>
+       
+
+       
       </div>
     </section>
   );
